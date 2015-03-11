4d0a091bf840a12207e707a2f3b9b596b06d880c
Dimitri Walters <me@dimitriwalters.com>
2015-03-10 22:45:21 -0400

    Use user service

--- hello.go
+++ hello.go
@@ -1,14 +1,29 @@
 package hello
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+
+	"appengine"
+	"appengine/user"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello, %v!", u)
 }
